c57994061d575b7bb231a469d53eb83a978fc98f
Francis Murillo <francismurillo@digix.global>
2019-07-11 09:04:11 +0800

    [fix] Add better suport for docker-compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -e
 
+echo "Creating database if it's not present..."
+bin/rails db:create
+
+echo "Migrating database..."
+bin/rails db:migrate
+
+if ! bundle check; then
+    echo "Updating dependencies..."
+    bundle install
+fi
+
 rm -f tmp/pids/server.pid
 
 exec "$@"
