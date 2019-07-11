0ae2509b9a04b0686758a346ef951a8c9231b8fe
Francis Murillo <francismurillo@digix.global>
2019-07-11 09:40:56 +0800

    [fix] Improve docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
+
 set -e
 
-echo "Creating database if it's not present..."
+echo "Creating database if not yet created..."
 bin/rails db:create
 
-echo "Migrating database..."
+echo "Migrating database if any pending migrations..."
 bin/rails db:migrate
 
-if ! bundle check; then
-    echo "Updating dependencies..."
-    bundle install
-fi
+echo "Updating dependencies if any uninstalled gems..."
+bundle install
 
 rm -f tmp/pids/server.pid
 
