eaf7246b669a47fb0316d5f8a6ed7e6c97c7a665
Francis Murillo <francismurillo@digix.global>
2019-07-11 10:59:28 +0800

    [fix] Configure puma.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :keep_releases, 5
 
 set :puma_threads, [4, 16]
 set :puma_workers, 0
-set :puma_bind, 'tcp://127.0.0.1:23000'
+set :puma_bind, ['tcp://0.0.0.0:23000']
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
